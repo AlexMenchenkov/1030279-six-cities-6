2fc79ac3830233661f7255ca286d40abea2b319a
amenchenkov <amenchenkov@livemaster.ru>
2021-02-03 20:15:36 +0300

    MOD: add dependencies

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Page from '/src/components/page/page.jsx';
+
+
+ReactDOM.render(
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <Page
+      title={`Keks`}
+    />,
+    document.getElementById(`root`)
+);

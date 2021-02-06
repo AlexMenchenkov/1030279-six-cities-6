10590d329cec856e82448d28365801133b8de730
amenchenkov <amenchenkov@livemaster.ru>
2021-02-06 13:38:23 +0300

    MOD: add route min

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import PropTypes from 'prop-types';
 import MainPage from '/src/components/main-page/main-page.jsx';
 import {propTypes} from '/src/components/card-place/card-place.jsx';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = (props) => {
   const {data} = props;
-  return <
-    MainPage data={data}
-  />;
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <
+            MainPage data={data}
+          />
+        </Route>
+        <Route path="/about" exact>
+          <
+            MainPage data={data}
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {

efe4b5f9dba9c4284543b31b87f5d3b13af3aa9d
amenchenkov <amenchenkov@livemaster.ru>
2021-02-07 21:33:52 +0300

    MOD: fix style

--- src/components/card-place/card-place.jsx
+++ src/components/card-place/card-place.jsx
@@ -33,7 +33,7 @@ export const CardPlace = (props) => {
       </div>
       <div className="place-card__rating rating">
         <div className="place-card__stars rating__stars">
-          <span style={{width: room.rate}}> </span>
+          <span style={{width: `${room.rate}%`}}> </span>
           <span className="visually-hidden">Rating</span>
         </div>
       </div>

b674aad616fc63ac6e16efb04958cfc93dbceb7e
amenchenkov <amenchenkov@livemaster.ru>
2021-02-10 15:59:29 +0300

    MOD: fix propTypes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '/src/components/app/app.jsx';
-import offers from './data.js';
+import offers from '/src/mocks/offers.js';
 
 ReactDOM.render(
     <App

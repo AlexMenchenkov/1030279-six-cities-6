c8368cbcf5b4f36e2c0f0c5243f9abedec8d9412
amenchenkov <16Elf@mail.ru>
2021-03-29 21:37:57 +0300

    MOD: fix filter

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import FilterSection from '/src/components/filter-section/filter-section';
 import {sectionsNames} from '/src/consts';
 import {connect} from "react-redux";
@@ -6,14 +6,24 @@ import {showFilter} from '/src/store/action';
 import {props} from './filter-prop';
 
 const Filter = ({sortId, showFilterPanel, showFilterDispatch}) => {
+  const idFilterNode = `filter`;
 
-  const handleSortClick = () => {
-    showFilterDispatch(!showFilterPanel);
+  const handleSortClick = (event) => {
+    if (event.target.id === idFilterNode) {
+      showFilterDispatch(!showFilterPanel);
+    } else if (showFilterPanel) {
+      showFilterDispatch(!showFilterPanel);
+    }
   };
+
+  useEffect(() => {
+    document.addEventListener(`click`, handleSortClick);
+    return () => document.removeEventListener(`click`, handleSortClick);
+  }, [showFilterPanel]);
 
   return (<form className="places__sorting" action="#" method="get">
     <span style={{marginRight: `10px`}} className="places__sorting-caption">Sort by</span>
-    <span onClick={handleSortClick} className="places__sorting-type" tabIndex="0">
+    <span id={idFilterNode} className="places__sorting-type" tabIndex="0">
       {sectionsNames[sortId]}
       <svg className="places__sorting-arrow" width="7" height="4">
         <use xlinkHref="#icon-arrow-select"> </use>
